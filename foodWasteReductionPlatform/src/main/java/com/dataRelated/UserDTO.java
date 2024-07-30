8d6a94647052d5ad702e08c895a1e27d34a2a341
jiaHongShih <shih0010@algonquinlive.com>
2024-07-30 16:09:13 -0400

    dto and stuff
    dto ig

---
+++ foodWasteReductionPlatform/src/main/java/com/dataRelated/UserDTO.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.dataRelated;
+
+/**
+ *
+ * @author JiaHong
+ */
+public class UserDTO {
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private UserType userType;
+    private String location;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUserType(UserType userType) {
+        this.userType = userType;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public UserType getUserType() {
+        return userType;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+}
---
+++ foodWasteReductionPlatform/src/main/java/com/dataRelated/UserType.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.dataRelated;
+
+/**
+ *
+ * @author JiaHong
+ */
+public enum UserType {
+    RETAILER,
+    CONSUMER,
+    CHARITABLE_ORGANIZATION
+}
